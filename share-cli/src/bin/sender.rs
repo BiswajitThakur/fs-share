a0489b46a054cf2fcb6b560bb2d3eeba4cdc01e4
BiswajitThakur <biswajit733127@gmail.com>
2024-10-27 11:02:20 +0530

    -

--- share-cli/src/bin/sender.rs
+++ share-cli/src/bin/sender.rs
@@ -1,6 +1,7 @@
 use std::{
     io::{self, Write},
     net::{SocketAddr, TcpStream},
+    time::Duration,
 };
 
 use share_utils::get_receiver_addr;
@@ -8,6 +9,7 @@ use share_utils::get_receiver_addr;
 fn main() {
     if let Ok(addr) = get_receiver_addr("Eagle BT", b"password") {
         println!("Reaciever addr: {:#?}", addr);
+        std::thread::sleep(Duration::from_secs(2));
         sender(addr.receiver).unwrap();
     }
 }
